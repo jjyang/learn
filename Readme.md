080f4e60909fd72ac85d210f6d76eb445e064969
JJYANG <jjyang.tw@gmail.com>
2014-08-05 02:33:58 +0800

    ok lah

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 This is readme
+
+Wa li ley
--- p.c
+++ p.c
@@ -1,5 +1,20 @@
 
+/* add new comment */
 function a(b, c)
 {
-  return b+c;
+  if ( b > 0 ) {
+    return b+c;
+  } else {
+    return b+c;
+  }
+}
+
+/* add mod comment */
+function mod(b, c)
+{
+  if ( b > 0 ) {
+    c = c+1;
+    return b % c;
+  }
+  return 0;
 }
